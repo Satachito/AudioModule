990f2622c4f63627d680b052acb6ef4403b61a3b
Satoru Ogura <satoru.ogura@me.com>
2017-11-04 16:15:43 +0900

    Initial Commit

---
+++ AudioModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioModule
+//
+//  Created by Satoru Ogura on 2017/11/04.
+//  Copyright © 2017年 Satoru Ogura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
